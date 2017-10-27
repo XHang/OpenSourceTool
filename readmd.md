1a041376848ab569f9afe6be704f2745a6af8862
Mr-hang <1083594261@qq.com>
2017-10-27 21:36:22 +0800

    XStreamd的用法

--- readmd.md
+++ readmd.md
@@ -2,6 +2,13 @@
 包含一下内容
 ## XML的学习和解析
 
+### XStream的基本使用
+XStream主要是xml和javaBean之间的互转。
+你需要注意的
+1. 除非要转的Bean类和XStream在同一个包内，否则需要为设置别名  
+	eg：`xStream.alias("userlist", List.class);`  
+	如果不设置，会报ClassNoneFoundException  
+ 
 ### 你知道吗
 1. Element和Node的区别：Element是继承自Node的。  
 	Node有三种：  
--- src/test/java/com/cxh/xml/BaseXMLUsage.java
+++ src/test/java/com/cxh/xml/BaseXMLUsage.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.Iterator;
+import java.util.List;
 import org.dom4j.Document;
 import org.dom4j.DocumentException;
 import org.dom4j.DocumentHelper;
@@ -12,6 +13,9 @@ import org.dom4j.Element;
 import org.dom4j.Node;
 import org.dom4j.io.SAXReader;
 import org.junit.Test;
+import com.cxh.bean.User;
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.io.xml.DomDriver;
 
 /**
  * 基本xml解析
@@ -50,6 +54,48 @@ public class BaseXMLUsage {
 		System.out.println("我们选取的标签值是"+node.getStringValue());
 	}
 	
+	@Test
+	public void XMLToObject() throws IOException{
+		InputStream in = this.getClass().getResourceAsStream("/example.xml");
+		Document document = BaseXMLAnalysis.genDocument(in);
+		Node usersNode = document.selectSingleNode("//userlist");
+		//xStream默认使用XppDriver解析，总是报错Cannot create XmlPullParser
+		//故换成DomDriver
+		XStream xStream  = new XStream(new DomDriver());
+		//解析成List集合
+		xStream.alias("userlist", List.class);
+		//把user标签以下的内容解析成User类的内容
+		xStream.alias("user", User.class);
+		//实例化成user时，把里面xml的appraisal标签转换成user的desc字段
+		xStream.aliasField("appraisal", User.class, "desc");
+		String xml = usersNode.asXML();
+		System.out.println("需要转成对象的xml字符串是"+xml);
+		List <User> users = (List<User>) xStream.fromXML(xml);
+		for(User user:users){
+			System.out.println("now read User");
+			System.out.println("The user's name :"+user.getName());
+			System.out.println("The user's sex :"+user.getSex());
+			System.out.println("The user's nationnality :"+user.getNationality());
+			System.out.println("The user's desc :"+user.getDesc());
+		}
+		ObjectToXml(users);
+		in.close();
+	}
+	
+	/**
+	 * 将对象转成xml
+	 * @param users
+	 */
+	public void ObjectToXml(List <User> users){
+		XStream xStream  = new XStream(new DomDriver());
+		//将对象中的desc字段名映射到xml的appraisal标签
+		xStream.aliasField("appraisal", User.class, "desc");
+		//如果不加这下面的两个的话，生成的xml文件头标签是类名
+		xStream.alias("user", User.class);
+		xStream.alias("userList", List.class);
+		System.out.println("下面将list对象转成对象");
+		System.out.println(xStream.toXML(users));
+	}
 	
 	
 }
