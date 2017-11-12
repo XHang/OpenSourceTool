0259a8f81a4a74477ddaa867eb9b37179cf57e18
Mr-hang <1083594261@qq.com>
2017-11-12 20:09:16 +0800

    POI Tbale Base Usage

--- src/main/java/com/cxh/wordApplication/WordApplication.java
+++ src/main/java/com/cxh/wordApplication/WordApplication.java
@@ -11,23 +11,5 @@ import org.apache.poi.xwpf.usermodel.XWPFTableRow;
 public class WordApplication {
 	
 	public static  void createWordFile() throws Exception{
-		XWPFDocument dox = new XWPFDocument();
-		dox.createStyles();
-		XWPFTable table = dox.createTable();
-		XWPFTableRow row= table.createRow();
-		XWPFTableCell cell =row.addNewTableCell();
-		cell.setText("我就是天才");
-		XWPFTableRow row1= table.createRow();
-		XWPFTableCell cell5 =row.addNewTableCell();
-		cell.setText("我就是天才");
-		XWPFTableRow row2= table.createRow();
-		XWPFTableCell cell3 =row.addNewTableCell();
-		cell.setText("我就是天才");
-		XWPFTableRow row3= table.createRow();
-		XWPFTableCell cell4 =row.addNewTableCell();
-		cell.setText("我就是天才");
-		OutputStream os = new FileOutputStream("D://1.doxc");
-		dox.write(os);
-		os.close();
 	}
 }
--- src/test/java/com/cxh/WordApplication/WordApplicationTest.java
+++ src/test/java/com/cxh/WordApplication/WordApplicationTest.java
@@ -1,12 +1,36 @@
 package com.cxh.WordApplication;
 
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.XWPFTable;
 import org.junit.Test;
-import com.cxh.wordApplication.WordApplication;
 
 public class WordApplicationTest {
 	
+	/**
+	 * 目标：创建一个word文档，往里面写一个表格，设计成4行一列
+	 * @throws Exception
+	 */
 	@Test
-	public void testGenerateWord() throws Exception{
-		WordApplication.createWordFile();
+	public void createTable() throws Exception{
+		XWPFDocument doc = new XWPFDocument();
+		XWPFTable table = doc.createTable();
+		table.getRow(0).getCell(0).setText("表格细胞1");
+		//以下创立3行，每一行有1列
+		for( int i=2;i<5;i++){
+			table.createRow().getCell(0).setText("表格细胞"+i);
+		}
+		
+		
+		OutputStream stream = new FileOutputStream("D:\\1.docx");
+		doc.write(stream);
+		stream.close();
+		doc.close();
+	}
+	
+	@Test
+	public void test(){
+		XWPFDocument doc = new XWPFDocument();
 	}
 }
